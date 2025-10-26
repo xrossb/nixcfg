a4cbed5bcfb46d28bdeede7b82519035cc2b5299
Edward Allen <hi@eallen.me>
2025-10-26 17:39:41 +1100

    Simplify portals config for niri

--- system/niri.nix
+++ system/niri.nix
@@ -15,22 +15,10 @@
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-gnome
     ];
-    config = {
-      common.default = [ "gtk" ];
-      niri = {
-        default = [
-          "gtk"
-          "gnome"
-        ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
-      };
-    };
   };
 
   environment.sessionVariables = {
