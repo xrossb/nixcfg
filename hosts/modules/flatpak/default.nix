fa8849bbef746f53413af6490b5ed023e9dd6922
Edward Allen <hi@eallen.me>
2025-10-28 20:25:46 +1100

    Fix unthemed cursor in Flatpaks

--- hosts/modules/flatpak/default.nix
+++ hosts/modules/flatpak/default.nix
@@ -8,6 +8,10 @@
     packages = [
       "io.github.kolunmi.Bazaar"
     ];
+    overrides = {
+      # Fix un-themed cursor in some Wayland apps.
+      global.Context.filesystems = "/run/current-system/sw/share/X11/fonts:ro;/nix/store:ro";
+    };
   };
 
   xdg.portal.enable = true;
