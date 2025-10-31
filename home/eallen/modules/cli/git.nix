43bb600338428b5f34a5182d2ef55fcd78ed64c4
Edward Allen <hi@eallen.me>
2025-11-01 00:43:33 +1100

    setup delta for git

--- home/eallen/modules/cli/git.nix
+++ home/eallen/modules/cli/git.nix
@@ -5,6 +5,28 @@
     settings = {
       user.email = "hi@eallen.me";
       user.name = "Edward Allen";
+
+      fetch.prune = true;
+      init.defaultBranch = "main";
+      pull.ff = "only";
+      push.default = "current";
+      rerere.enabled = true;
+
+      delta = {
+        navigate = true;
+        light = false;
+        line-numbers = true;
+        zero-style = "dim syntax normal";
+        plus-style = "syntax normal";
+        minus-style = "dim syntax dim auto";
+      };
+      diff.colorMoved = "default";
+      merge.conflictstyle = "diff3";
     };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
   };
 }
