367553502d632d97b7628a84359a5322eee0ff69
Edward Allen <hi@eallen.me>
2025-11-01 00:23:49 +1100

    setup commit signing on nixps

--- home/eallen/nixps.nix
+++ home/eallen/nixps.nix
@@ -10,4 +10,11 @@
   home.username = "eallen";
   home.homeDirectory = "/home/${config.home.username}";
   home.stateVersion = "25.05";
+
+  # Enable commit signing with the on-device GPG key.
+  programs.git.extraConfig = {
+    user.signingkey = "BA8B9B76E2A40F49";
+    commit.gpgsign = true;
+    tag.gpgSign = true;
+  };
 }
