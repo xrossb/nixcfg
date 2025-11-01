973c3bf2cd7663c3a0a8877952020dcc9132162c
Edward Allen <hi@eallen.me>
2025-11-01 13:07:18 +1100

    git autoSetupRemote; configure delta theme

--- home/eallen/modules/cli/git.nix
+++ home/eallen/modules/cli/git.nix
@@ -9,16 +9,20 @@
       fetch.prune = true;
       init.defaultBranch = "main";
       pull.ff = "only";
+      push.autoSetupRemote = true;
       push.default = "current";
       rerere.enabled = true;
 
       delta = {
+        features = "decorations";
+        syntax-theme = "OneHalfDark";
         navigate = true;
         light = false;
         line-numbers = true;
         zero-style = "dim syntax normal";
         plus-style = "syntax normal";
         minus-style = "dim syntax dim auto";
+        decorations.hunk-header-decoration-style = "none";
       };
       diff.colorMoved = "default";
       merge.conflictstyle = "diff3";
