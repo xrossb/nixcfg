59f2d153f5c3c2c9619bec6f7d554937851bc434
Edward Allen <hi@eallen.me>
2025-11-06 15:36:50 +1100

    add libsecret dylib

--- hosts/modules/common/default.nix
+++ hosts/modules/common/default.nix
@@ -1,9 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.config.allowUnfree = true;
 
   programs.nix-ld = {
     enable = true;
-    libraries = [];
+    libraries = with pkgs; [
+      libsecret
+    ];
   };
 }
