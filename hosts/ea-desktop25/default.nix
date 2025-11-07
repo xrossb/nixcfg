a2f87005b01c9a1c0867235ae8cbe0bb928348c9
Edward Allen <hi@eallen.me>
2025-11-08 00:38:06 +1100

    auto usb_modeswitch for logitech wheel

--- hosts/ea-desktop25/default.nix
+++ hosts/ea-desktop25/default.nix
@@ -56,4 +56,7 @@
   hardware.usb-modeswitch.enable = true;
   hardware.new-lg4ff.enable = true;
   services.udev.packages = with pkgs; [oversteer];
+  services.udev.extraRules = ''
+    ATTR{idVendor}=="046d", ATTR{idProduct}=="c26d", RUN+="${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 046d -p c26d -M 0f00010142 -C 0x03 -m 01 -r 81"
+  '';
 }
