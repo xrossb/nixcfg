c9be92e9d2194837c05269d9a037faa2fc001a6a
Edward Allen <hi@eallen.me>
2026-03-29 17:56:42 +1100

    include ts,tsx config for helix

--- home/eallen/modules/cli/helix.nix
+++ home/eallen/modules/cli/helix.nix
@@ -1,4 +1,14 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  nil = lib.getExe pkgs.nil;
+  alejandra = lib.getExe pkgs.alejandra;
+  typescript-language-server = lib.getExe pkgs.nodePackages.typescript-language-server;
+  eslint = "${pkgs.nodePackages.eslint}/bin/eslint";
+  prettier = lib.getExe pkgs.nodePackages.prettier;
+in {
   programs.helix = {
     enable = true;
     defaultEditor = true;
@@ -25,12 +35,43 @@
         "C-space" = "completion";
       };
     };
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-        formatter = {command = "alejandra";};
-      }
-    ];
+    languages = {
+      language-server = {
+        nil.command = nil;
+
+        typescript-language-server.command = typescript-language-server;
+        eslint = {
+          command = eslint;
+          args = ["--stdin"];
+        };
+      };
+
+      language = [
+        {
+          name = "nix";
+          language-servers = ["nil"];
+          formatter.command = alejandra;
+          auto-format = true;
+        }
+        {
+          name = "typescript";
+          language-servers = ["typescript-language-server" "eslint"];
+          formatter = {
+            command = prettier;
+            args = ["--parser" "typescript"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "tsx";
+          language-servers = ["typescript-language-server" "eslint"];
+          formatter = {
+            command = prettier;
+            args = ["--parser" "typescript"];
+          };
+          auto-format = true;
+        }
+      ];
+    };
   };
 }
