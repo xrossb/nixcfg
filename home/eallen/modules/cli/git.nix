66251ffc12d381697645e7d1e3d70d1b4228964a
Edward Allen <hi@eallen.me>
2026-04-19 14:30:26 +1000

    add some old git aliases

--- home/eallen/modules/cli/git.nix
+++ home/eallen/modules/cli/git.nix
@@ -1,4 +1,11 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  fzf = lib.getExe pkgs.fzf;
+  delta = lib.getExe pkgs.delta;
+in {
   programs.git = {
     enable = true;
     lfs.enable = true;
@@ -26,6 +33,26 @@
       };
       diff.colorMoved = "default";
       merge.conflictstyle = "diff3";
+
+      alias = {
+        cleanout = "!git clean -df && git restore .";
+        default = "!git remote show origin | sed -n '/HEAD branch/s/.*: //p'";
+        grab = ''
+          !git branch -r --sort=committerdate --format='%(refname:lstrip=2)' \
+            | ${fzf} --preview='git diff HEAD...{1} | ${delta}' --preview-window='right:70%' \
+            | sed 's/origin\\///' \
+            | xargs git switch
+        '';
+        recent = ''
+
+        '';
+        unwip = "!git log -n 1 | grep -q -c wip && git reset HEAD~1";
+        wip = ''
+          !git add --all; \
+          git ls-files --deleted -z | xargs -r -0 git rm; \
+          git commit --message=wip
+        '';
+      };
     };
   };
 
