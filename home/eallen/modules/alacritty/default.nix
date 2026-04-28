34f9132ec075bd685f5e669c92807069f9b70ffd
Edward Allen <hi@eallen.me>
2026-04-28 11:02:04 +1000

    format alacritty.nix

--- home/eallen/modules/alacritty/default.nix
+++ home/eallen/modules/alacritty/default.nix
@@ -2,8 +2,16 @@
   programs.alacritty = {
     enable = true;
     theme = "ayu_dark";
+
     settings = {
       terminal.shell = "tmux";
+      env.TERM = "xterm-256color"; # "alacritty" isn't recognised by a lot of CLI tools.
+
+      font = {
+        normal.family = "CaskaydiaCove Nerd Font Mono";
+        size = 13;
+      };
+
       window = {
         padding = {
           x = 4;
@@ -11,12 +19,8 @@
         };
         dynamic_padding = true;
       };
-      font = {
-        normal.family = "CaskaydiaCove Nerd Font Mono";
-        size = 13;
-      };
+
       mouse.hide_when_typing = true;
-      env.TERM = "xterm-256color"; # "alacritty" isn't recognised by a lot of CLI tools.
     };
   };
 }
