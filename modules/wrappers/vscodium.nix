6ffaaf21ae07666c0d8669f0a178f3b02c58a1f1
Edward Allen <hi@eallen.me>
2026-06-04 11:59:26 +1000

    add go + rust tools to vscodium

--- modules/wrappers/vscodium.nix
+++ modules/wrappers/vscodium.nix
@@ -7,8 +7,22 @@
     imports = [wlib.modules.default];
     package = pkgs.vscodium;
     runtimePkgs = with pkgs; [
+      # go
+      delve
+      impl
+      go
+      go-tools
+      gotools
+      gopls
+      gotests
+
+      # nix
       alejandra
       nixd
+
+      # rust
+      rust-analyzer
+      rustfmt
     ];
   };
 }
