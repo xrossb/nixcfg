c34ea50873ddc817b0796943e9ea5429e757e95a
Edward Allen <hi@eallen.me>
2026-06-09 13:47:54 +1000

    use flake.wrappers for ironbar

--- modules/wrappers/ironbar/default.nix
+++ modules/wrappers/ironbar/default.nix
@@ -1,37 +1,39 @@
-{inputs, ...}: {
-  perSystem = {pkgs, ...}: {
-    packages.ironbar = inputs.wrappers.lib.wrapPackage ({...}: let
-      config = {
-        position = "top";
-        height = 30;
-        layer = "overlay";
-        popup_autohide = true;
-        start = [
-          {type = "launcher";}
-          {type = "workspaces";}
-        ];
-        center = [
-          {
-            type = "clock";
-            format = "%a, %d %b · %-l:%M %P";
-          }
-        ];
-        end = [
-          {type = "tray";}
-          {type = "battery";}
-          {type = "volume";}
-          {type = "bluetooth";}
-          {type = "network_manager";}
-          {type = "menu";}
-        ];
-      };
-    in {
-      inherit pkgs;
-      package = pkgs.ironbar;
-      env = {
-        IRONBAR_CSS = toString ./style.css;
-        IRONBAR_CONFIG = builtins.toFile "config.json" (builtins.toJSON config);
-      };
-    });
+{...}: {
+  flake.wrappers.ironbar = {
+    pkgs,
+    wlib,
+    ...
+  }: let
+    config = {
+      position = "top";
+      height = 30;
+      layer = "overlay";
+      popup_autohide = true;
+      start = [
+        {type = "launcher";}
+        {type = "workspaces";}
+      ];
+      center = [
+        {
+          type = "clock";
+          format = "%a, %d %b · %-l:%M %P";
+        }
+      ];
+      end = [
+        {type = "tray";}
+        {type = "battery";}
+        {type = "volume";}
+        {type = "bluetooth";}
+        {type = "network_manager";}
+        {type = "menu";}
+      ];
+    };
+  in {
+    imports = [wlib.modules.default];
+    package = pkgs.ironbar;
+    env = {
+      IRONBAR_CSS = toString ./style.css;
+      IRONBAR_CONFIG = builtins.toFile "config.json" (builtins.toJSON config);
+    };
   };
 }
