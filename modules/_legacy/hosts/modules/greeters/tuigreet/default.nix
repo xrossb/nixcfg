8519c7801aa03107fc7181847578e262d9436bfa
Edward Allen <hi@eallen.me>
2026-09-14 15:26:25 +1000

    filter nixbld users from tuigreet; disable --user-menu for now

--- modules/_legacy/hosts/modules/greeters/tuigreet/default.nix
+++ modules/_legacy/hosts/modules/greeters/tuigreet/default.nix
@@ -5,15 +5,18 @@
 }: {
   services.greetd = {
     enable = true;
+    useTextGreeter = true;
     settings = {
       default_session = {
         command = lib.concatStringsSep " " [
           (lib.getExe pkgs.tuigreet)
           "--time"
           "--asterisks"
-          "--user-menu"
+          # "--user-menu" # TODO: enable when compatible w/ --remember again.
+          "--user-menu-min-uid=1000"
+          "--user-menu-max-uid=29999" # ignore nixbld users.
           "--remember"
-          "--prompt-padding 0"
+          "--prompt-padding=0"
         ];
         user = "greeter";
       };
